5c5a03e4e6e619295c49c423498de7986f24c53c
Doumit <doumit.beaino@epita.fr>
2022-05-03 19:27:28 +0200

    basics_done

---
+++ inventory_app/dashboard/models.py
@@ -0,0 +1,13 @@
+from sre_parse import CATEGORIES
+from unicodedata import category
+from django.db import models
+
+
+# Create your models here.
+
+class Item (models.Model):
+    name = models.CharField(max_length=100, null=True)
+    quantity = models.IntegerField()
+
+def __str__(self):
+        return f'{self.name}{self.quantity}'
